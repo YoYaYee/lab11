e6ab11d82091b0bed8f0b5be853f5fedb1c402cd
YoYaYee <eden555007@gmail.com>
2024-12-20 14:46:45 +0700

    lab11_1_complete.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+string grade[] = {"A","B+","B","C+","C","D+","D","F","W"};
+int i;
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    while(i <= 2){
+    cin.get();
+    i++;
+    }
+    srand(time(0));
+    int x = rand()%9;
+    cout << "You will get " << grade[x] << " in this 261102.";
+    return 0;
+}
